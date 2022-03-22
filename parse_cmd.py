e537b657f8f9e806124cbb0693d8d5b2fc188e59
Derek <trackmenot10@gmail.com>
2022-03-21 21:07:42 -0700

    Improved the parser. It is now as good as it needs to be.

--- parse_cmd.py
+++ parse_cmd.py
@@ -39,70 +39,55 @@ class InvalidArgument(Exception): ...
 class InvalidSyntax(Exception): ...
 
 
-def parse(parser, msg):
-
+def parse(parser: argparse.Namespace, msg: str):
+    '''Parses a !cmd add or !cmd edit command
+    and returns new data and message.'''
     args = msg.split()
     if len(args) < 3:
+        if '-h' in args or '--help' in args:
+            return parser.print_help()
         return f"Syntax Error: Not enough arguments. <!cmd syntax info>"
-        
-    num_args = 1
+    num_parsed = 1
     last_result: argparse.Namespace = None
-    valid_flags = ('--permissions', '-p', '--aliases', '-a', '--count', '-c', '--hide', '-i', '--disable', '-d',  ) # '--invisible', '-i')
+    valid_flags = ('--help', '-h', '--permissions', '-p', '--aliases', '-a', '--count', '-c', '--hide', '-i', '--disable', '-d',  )
     if args[0] == 'edit':
-        valid_flags += ('--rename', '-r', '--enable', '-e', '--unhide', '-u', ) # '--visible', '-v')
+        valid_flags += ('--rename', '-r', '--enable', '-e', '--unhide', '-u', )
 
-    # print(args)
     for i, _ in enumerate(args):
-        #if i < 3:
-            #continue
-        # print(i, args[i])
-
-
-
         try:
+            # Go ahead and parse the command if it is the minimum length:
             if len(args) == 3 and args[-1] in valid_flags:
                 return parser.parse_args(args)
             last_result = parser.parse_args(args[:i+1])
-            num_args += 1
-
+            num_parsed += 1
         except SystemExit as exc:
-            # print("SysEx")
-            #print(last_result)
-            # Is the next thing not a valid arg, meaning the beginning of the message?
+            # Catch the help flag:
+            if args[i] in ('--help', '-h'):
+                return parser.print_help()
+            # Is the next pair of args invalid, meaning the beginning of the message?
             if not any(a in valid_flags for a in args[i+1:i+3]):
-                print("!!! No flags in next 2 words")
+                # Disregard if this is just the command name:
                 if i < 3:
-                    print("Continuing...")
                     continue
                 break
-                #print(args[i])
-                # if args[i] in valid_flags:
-                    # print(args[i], "is a flag")
-                    #num_args += 1
-                #return last_result, msg.split(None, num_args)[-1]
-            # if args[i+1] in valid_flags:
-                # print("Found a flag in next arg...")
-                # continue
-            elif i > 3:
-                # print("Next arg is not a valid flag...")
-                return InvalidArgument(f"Unrecognized argument: {args[i]!r}")
+            # If not a legitimate command name, catch this as an invalid arg:
+            if i > 2:
+                return InvalidArgument(f"Invalid argument: {args[i]!r}")
 
         except argparse.ArgumentError as exc:
-            # print("ArgErr")
-            if args[i] not in valid_flags:
-                return InvalidSyntax(f"Syntax error: {exc}")
-            #elif i == len(args) - 1:
-                #return exc
+            # If the command lacks a message, begins with anything that can be interpreted
+            # as a flag, it will be considered a syntax error:
+            #if args[i] not in valid_flags:
+            #i == len(args) - 1
             if i == len(args) - 1 or args[i] not in valid_flags:
                 return InvalidArgument(f"Syntax error: {exc}")
-            num_args += 1
+            num_parsed += 1
 
-    # if not (num_args == len(args) and args[num_args] in valid_flags):
-    # print("End of loop")
-    # print(f"{num_args=}, {len(args)=}")
     if not last_result:
-        num_args += 1
-    if len(args) == num_args:
+        num_parsed += 1
+    # Do not attempt to include the message if every
+    # argument in the command was parsed successfully:
+    if len(args) == num_parsed:
         return last_result
-    return last_result, msg.split(None, num_args)[-1]
+    return last_result, msg.split(None, num_parsed)[-1]
 
