ef3d0c0c4ee8c5b21e93c0e2807eb47d482bb188
Derek <trackmenot10@gmail.com>
2022-04-03 22:49:17 -0700

    feat(command): Use bools for cond args
    I discovered that sending True or False values to the sqlite database converts them to ints automatically, eliminating the need to store const values for optional arguments. Next I must go in and change the typings for these attributes in base_classes.

--- parse_cmd.py
+++ parse_cmd.py
@@ -5,43 +5,74 @@ parser = argparse.ArgumentParser(prog='!cmd', description='CMD DESC', exit_on_er
 subparsers = parser.add_subparsers(help="Help for all subcommands here.")
 
 cmd_add_or_edit = subparsers.add_parser('!cmd', add_help=False)
+# defaults = dict.fromkeys((
+    # 'perms',
+    # 'aliases',
+    # 'count',
+    # 'is_enabled',
+    # 'is_hidden',
+    # 'override_builtin'
+    # ), argparse.SUPPRESS)
+# cmd_add_or_edit.set_defaults(**defaults)
 
-#args = {
-#    'perms': (('--perms', '-'), {'choices': "everyone vip moderator owner rank=".split(), 'type': lambda s: s.lower()}),
-#    'aliases': (('--aliases', '-a'), {'nargs': 1}),
-#    'count': (('--count', '-c'), {'type': int}),
-#    'disable': (('--disable', '-d'), {'action': 'store_const', }),
-#    'hidden': (('--hidden', '-i'), {}),
-#    'override': (('--override_builtin'), {}),
-#    'enable': (('--enable', 'e'), {}),
-#    'unhidden': (('--unhidden', '-u'), {}),
-#    'rename': (('--rename', '-r'), {}),
-#}
+# args = {
+    # 'perms': (('--perms', '-'), {'choices': "everyone vip moderator owner rank=".split(), 'type': lambda s: s.lower()}),
+    # 'aliases': (('--aliases', '-a'), {'nargs': 1}),
+    # 'count': (('--count', '-c'), {'type': int}),
+    # 'disable': (('--disable', '-d'), {'action': 'store_const', }),
+    # 'hidden': (('--hidden', '-i'), {}),
+    # 'override': (('--override_builtin'), {}),
+    # 'enable': (('--enable', 'e'), {}),
+    # 'unhidden': (('--unhidden', '-u'), {}),
+    # 'rename': (('--rename', '-r'), {}),
+# }
 
 
 cmd_add_or_edit.add_argument('name', nargs=1)
-cmd_add_or_edit.add_argument('--perms', '-p', choices="everyone vip moderator owner rank=".split(), type=lambda s: s.lower())
+cmd_add_or_edit.add_argument('--perms', '-p',
+    choices="everyone vip moderator owner rank=".split(),
+    type=lambda s: s.lower())
 cmd_add_or_edit.add_argument('--aliases', '-a', nargs=1)
 cmd_add_or_edit.add_argument('--count', '-c', type=int)
-cmd_add_or_edit.add_argument('--disable', '-d', action='store_const', const=0, dest='is_enabled')
-cmd_add_or_edit.add_argument('--hidden', '-i', action='store_const', const=1, dest='is_hidden')
-cmd_add_or_edit.add_argument('--override_builtin', action='store_const', const=1)
+cmd_add_or_edit.add_argument('--disable', '-d', action='store_false', dest='is_enabled')
+cmd_add_or_edit.add_argument('--hidden', '-i', action='store_true', dest='is_hidden')
+cmd_add_or_edit.add_argument('--override_builtin', action='store_true')
 
-cmd_add = subparsers.add_parser('add', parents=[cmd_add_or_edit], exit_on_error=False, description="Add a new custom command.", help="ADD HELP")
 
-cmd_edit = subparsers.add_parser('edit', parents=[cmd_add_or_edit], exit_on_error=False, description="Edit a custom command's message and properties.", help="EDIT HELP")
+cmd_add = subparsers.add_parser('add',
+    parents=[cmd_add_or_edit], exit_on_error=False,
+    description="Add a new custom command.", help="ADD HELP")
+cmd_add_defaults = {
+    'perms': 'everyone',
+    'is_hidden': 0,
+    'is_enabled': 1
+}
+cmd_add.set_defaults(**cmd_add_defaults)
 
-cmd_edit.add_argument('--enable', '-e', action='store_const', const=1, dest='is_enabled')
-cmd_edit.add_argument('--unhidden', '-u', action='store_const', const=0, dest='is_hidden')
+cmd_edit = subparsers.add_parser('edit', parents=[cmd_add_or_edit],
+    exit_on_error=False,
+    description="Edit a custom command's message and properties.",
+    help="EDIT HELP")
+
+cmd_edit.add_argument('--enable', '-e', action='store_true', dest='is_enabled')
+cmd_edit.add_argument('--unhidden', '-u', action='store_false', dest='is_hidden')
 cmd_edit.add_argument('--rename', '-r', nargs=1, dest='new_name')
 
 other_actions = subparsers.add_parser('!cmd', add_help=False)
+# other_actions.set_defaults(
+    # **{'exit_on_error': False,
+    # 'description': "PLACEHOLDER DESC",
+    # 'help': "PLACEHOLDER HELP" })
 other_actions.add_argument('commands', nargs='+')
 
-cmd_delete = subparsers.add_parser('delete', parents=[other_actions], exit_on_error=False, description="Delete commands.", help="Multiple commands may be deleted at once.")
-cmd_disable = subparsers.add_parser('disable', parents=[other_actions], exit_on_error=False, description="Disable commands.", help="Multiple commands may be disabled at once.")
-cmd_enable = subparsers.add_parser('enable', parents=[other_actions], exit_on_error=False, description="Enable commands.", help="Multiple commands may be enabled at once.")
-cmd_alias = subparsers.add_parser('alias', parents=[other_actions], exit_on_error=False, description="Set command aliases.", help="Specify one or more aliases for a given command.")
+cmd_delete = subparsers.add_parser('delete', parents=[other_actions],
+    exit_on_error=False, description="Delete commands.", help="Multiple commands may be deleted at once.")
+cmd_disable = subparsers.add_parser('disable', parents=[other_actions],
+    exit_on_error=False, description="Disable commands.", help="Multiple commands may be disabled at once.")
+cmd_enable = subparsers.add_parser('enable', parents=[other_actions],
+    exit_on_error=False, description="Enable commands.", help="Multiple commands may be enabled at once.")
+cmd_alias = subparsers.add_parser('alias', parents=[other_actions],
+    exit_on_error=False, description="Set command aliases.", help="Specify one or more aliases for a given command.")
 
 
 class InvalidArgument(Exception): ...
@@ -108,5 +139,6 @@ def parse(msg: str, parser: argparse.ArgumentParser = parser):
     # argument in the command was parsed successfully:
     if len(args) == num_parsed:
         return last_result
+    # print(last_result)
     return last_result, msg.split(None, num_parsed)[-1]
 
