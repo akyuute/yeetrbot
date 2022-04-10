9b7d9fc4b12e613949e5d20378350635b4a72b49
Derek <trackmenot10@gmail.com>
2022-04-09 17:33:57 -0700

    Added parsing.py

--- parsing.py
+++ parsing.py
@@ -30,6 +30,7 @@ parser = QuietParser(prog='!cmd', description='CMD DESC', exit_on_error=False)
 subparsers = parser.add_subparsers(help="Help for all subcommands here.")
 
 cmd_add_or_edit = subparsers.add_parser('!cmd', add_help=False)
+<<<<<<< HEAD
 <<<<<<< HEAD:parse_cmd.py
 # defaults = dict.fromkeys((
     # 'perms',
@@ -56,6 +57,8 @@ cmd_add_or_edit = subparsers.add_parser('!cmd', add_help=False)
 
 =======
 >>>>>>> exper-dataclasses:parsing.py
+=======
+>>>>>>> exper-dataclasses
 cmd_add_or_edit.add_argument('name', nargs=1)
 cmd_add_or_edit.add_argument('--perms', '-p',
     choices="everyone vip moderator owner rank=".split(),
@@ -64,6 +67,7 @@ cmd_add_or_edit.add_argument('--aliases', '-a', nargs=1)
 cmd_add_or_edit.add_argument('--count', '-c', type=int)
 cmd_add_or_edit.add_argument('--disable', '-d', action='store_false', dest='is_enabled')
 cmd_add_or_edit.add_argument('--hidden', '-i', action='store_true', dest='is_hidden')
+<<<<<<< HEAD
 <<<<<<< HEAD:parse_cmd.py
 cmd_add_or_edit.add_argument('--override_builtin', action='store_true')
 =======
@@ -84,19 +88,29 @@ cmd_add.set_defaults(**cmd_add_defaults)
 
 =======
 >>>>>>> exper-dataclasses:parsing.py
+=======
+# cmd_add_or_edit.add_argument('--override_builtin', action='store_true')
+
+cmd_add = subparsers.add_parser('add', parents=[cmd_add_or_edit], exit_on_error=False, description="Add a new custom command.", help="ADD HELP")
+
+>>>>>>> exper-dataclasses
 cmd_edit = subparsers.add_parser('edit', parents=[cmd_add_or_edit],
     exit_on_error=False,
     description="Edit a custom command's message and properties.",
     help="EDIT HELP")
+<<<<<<< HEAD
 <<<<<<< HEAD:parse_cmd.py
 
 =======
 >>>>>>> exper-dataclasses:parsing.py
+=======
+>>>>>>> exper-dataclasses
 cmd_edit.add_argument('--enable', '-e', action='store_true', dest='is_enabled')
 cmd_edit.add_argument('--unhidden', '-u', action='store_false', dest='is_hidden')
 cmd_edit.add_argument('--rename', '-r', nargs=1, dest='new_name')
 
 other_actions = subparsers.add_parser('!cmd', add_help=False)
+<<<<<<< HEAD
 # other_actions.set_defaults(
     # **{'exit_on_error': False,
     # 'description': "PLACEHOLDER DESC",
@@ -113,11 +127,18 @@ cmd_enable = subparsers.add_parser('enable', parents=[other_actions],
 cmd_alias = subparsers.add_parser('alias', parents=[other_actions],
     exit_on_error=False, description="Set command aliases.", help="Specify one or more aliases for a given command.")
 =======
+=======
+other_actions.add_argument('commands', nargs='+')
+
+>>>>>>> exper-dataclasses
 #cmd_delete = subparsers.add_parser('delete', parents=[other_actions], exit_on_error=False, description="Delete commands.", help="Multiple commands may be deleted at once.")
 #cmd_disable = subparsers.add_parser('disable', parents=[other_actions], exit_on_error=False, description="Disable commands.", help="Multiple commands may be disabled at once.")
 #cmd_enable = subparsers.add_parser('enable', parents=[other_actions], exit_on_error=False, description="Enable commands.", help="Multiple commands may be enabled at once.")
 #cmd_alias = subparsers.add_parser('alias', parents=[other_actions], exit_on_error=False, description="Set command aliases.", help="Specify one or more aliases for a given command.")
+<<<<<<< HEAD
 >>>>>>> exper-dataclasses:parsing.py
+=======
+>>>>>>> exper-dataclasses
 
 
 def parse_cmd(msg: str, parser: QuietParser = parser) -> tuple|Namespace:
@@ -175,6 +196,9 @@ def parse_cmd(msg: str, parser: QuietParser = parser) -> tuple|Namespace:
     # argument in the command was parsed successfully:
     if len(args) == num_parsed:
         return last_result
+<<<<<<< HEAD
     # print(last_result)
+=======
+>>>>>>> exper-dataclasses
     return last_result, msg.split(None, num_parsed)[-1]
 
