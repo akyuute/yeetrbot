f09b2cf2953f9234aac55accfd72db01c0a5024b
Derek <trackmenot10@gmail.com>
2022-04-16 03:43:18 -0700

    Fix overwrites; Remove help; Add valid flags

--- parsing.py
+++ parsing.py
@@ -1,10 +1,10 @@
 '''Functions for parsing config files, values and command strings.'''
 
 import argparse
-# from argparse import  ArgumentParser, REMAINDER, ArgumentError
-# from errors import ParsingIncomplete, InvalidArgument, InvalidSyntax
 from errors import ParsingError
 
+valid_parser_flags = ('--help', '-h', '--permissions', '-p', '--aliases', '-a',
+       '--count', '-c', '--disable', '-d', '--hide', '-i', '--unhide', '-u')
 
 class QuietParser(argparse.ArgumentParser):
     '''An `ArgumentParser` that doesn't spam stderr with usage messages.'''
@@ -13,38 +13,35 @@ class QuietParser(argparse.ArgumentParser):
 
 
 cmd_add_or_edit = QuietParser(prog='!cmd',
-    add_help=False,
-    )
+    add_help=False)
+cmd_add_or_edit.set_defaults(default=None)
 
-cmd_add_or_edit.add_argument('--help', '-h', action='store_true')
-# cmd_add_or_edit.add_argument('name', nargs=1)
+cmd_add_or_edit.add_argument('--help', '-h',
+        action='store_true',
+        default=argparse.SUPPRESS)
+
 cmd_add_or_edit.add_argument('--perms', '-p',
     choices="everyone vip moderator owner rank=".split(),
     type=lambda s: s.lower())
+
 cmd_add_or_edit.add_argument('--aliases', '-a', nargs=1)
 cmd_add_or_edit.add_argument('--count', '-c', type=int)
+
 cmd_add_or_edit.add_argument('--disable', '-d',
     action='store_false',
-    dest='is_enabled',
-    default=None
-    )
+    dest='is_enabled')
+
 cmd_add_or_edit.add_argument('--enable', '-e',
     action='store_true',
-    dest='is_enabled',
-    default=None
-    )
+    dest='is_enabled')
 
 cmd_add_or_edit.add_argument('--hide', '-i',
     action='store_true',
-    dest='is_hidden',
-    default=None
-    )
+    dest='is_hidden')
 
 cmd_add_or_edit.add_argument('--unhide', '-u',
     action='store_false',
-    dest='is_hidden',
-    default=None
-    )
+    dest='is_hidden')
 
 # cmd_add_or_edit.add_argument('--override_builtin', action='store_true')
 cmd_add_or_edit.add_argument('message', nargs=argparse.REMAINDER)
